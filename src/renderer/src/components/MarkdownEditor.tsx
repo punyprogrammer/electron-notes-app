35e782ab8e281701af9809bbd398145d45d37b6c
Amardeep Ganguly <amardeepganguly@Amardeeps-MacBook-Air.local>
2024-04-13 01:41:16 +0530

    add-auto-save+throttle

--- src/renderer/src/components/MarkdownEditor.tsx
+++ src/renderer/src/components/MarkdownEditor.tsx
@@ -8,13 +8,16 @@ import {
 import { useMarkdownEditor } from '../hooks/useMarkdownEditor'
 
 export const MarkdownEditor = () => {
-  const selectedNote = useMarkdownEditor()
+  const { selectedNote, handleAutoSave, editorRef, handleBlur } = useMarkdownEditor()
   if (!selectedNote) return null
   return (
     <MDXEditor
+      ref={editorRef}
       key={selectedNote.title + selectedNote.lastEditTime}
+      onChange={handleAutoSave}
       plugins={[headingsPlugin(), listsPlugin(), markdownShortcutPlugin(), quotePlugin()]}
       markdown={selectedNote.content}
+      onBlur={handleBlur}
       contentEditableClassName="outline-none min-h-screen max-w-none text-lg px-8 py-5 caret-yellow-500 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
     />
   )
